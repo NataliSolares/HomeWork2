a537e86d65229fe594548701240006df0005ed79
Natali <n_sunberry@inbox.ru>
2022-08-13 21:30:06 +0300

    9 Задача

---
+++ Practice6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму чисел от 1 до А
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int f = 1;
+int i = 1;
+while(i<=n)
+{
+    Console.WriteLine(f=f*i)
+    i++;
+}
---
+++ Practice9/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int f = 1;
+int i = 1;
+while (i <= n)
+{
+    f = f * i;
+    i++;
+}
+Console.WriteLine("Произведение: " + f);
