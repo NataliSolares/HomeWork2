411e6c029b71bbdefb4443f596926de0fa6553cb
Natali <n_sunberry@inbox.ru>
2022-08-13 20:01:30 +0300

    3задача

---
+++ Practice3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Введите номер четверти от 1 до 4: " );
+int a = int.Parse(Console.ReadLine()?? "0");
+if(a==1)
+{
+    Console.WriteLine("диапазон 1 четверти x>0 до infinity; y>0 до infinity");
+}
+if(a==2)
+{
+   Console.WriteLine("диапазон 2 четверти x>0 до -infinity; y>0 до infinity"); 
+}
+if(a==3)
+{
+    Console.WriteLine("диапазон 3 четверти x>0 до -infinity; y>0 до -infinity");
+}
+if(a==4)
+{
+    Console.WriteLine("диапазон 4 четверти x>0 до infinity; y>0 до -infinity");
+}
