f6ecbf5314af14f5acee9721137aa3d4e9ddaddb
Natali <n_sunberry@inbox.ru>
2022-08-13 20:50:12 +0300

    5 Задача

---
+++ Practice5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+while (i <= a)
+{
+    Console.Write(i*i*i + " ");
+    i++;
+}
