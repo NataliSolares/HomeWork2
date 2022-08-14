b849cbed7aad590c3b9516e67a488778658a76ac
Natali <n_sunberry@inbox.ru>
2022-08-14 22:47:07 +0300

    4 Задача

--- Practice4/Program.cs
+++ Practice4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти расстояние между точками в пространстве 2D/3D
+int x1 = 10;
+int x2 = 6;
+int y1 = 8;
+int y2 = 2;
+int z1 = 13;
+int z2 = 20;
+double d2 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+double d3 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+Console.WriteLine(d2);
+Console.WriteLine(d3);
