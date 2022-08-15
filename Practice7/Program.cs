b746a5add723f97bba5b575de057066e8896fd97
Natali <n_sunberry@inbox.ru>
2022-08-15 22:42:07 +0300

    7 Задача

--- Practice7/Program.cs
+++ Practice7/Program.cs
@@ -1,1 +1,10 @@
 ﻿// Определить количество цифр в числе
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int i = 0;
+while (n > 0)
+{
+    n = n/10;
+    i++;
+}
+Console.WriteLine("Количество цифр в числе: " + i);
