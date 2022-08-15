247998728f9f26528170d3b8973f86dd4b860f7d
Natali <n_sunberry@inbox.ru>
2022-08-15 22:12:32 +0300

    6 Задача

--- Practice6/Program.cs
+++ Practice6/Program.cs
@@ -1,10 +1,4 @@
 ﻿// Найти сумму чисел от 1 до А
 Console.WriteLine("Введите число: ");
-int n = int.Parse(Console.ReadLine() ?? "0");
-int f = 1;
-int i = 1;
-while(i<=n)
-{
-    Console.WriteLine(f=f*i)
-    i++;
-}
+int A = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Сумма чисел " + A * (A + 1) / 2);
