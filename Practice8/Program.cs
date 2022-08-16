8113bcdd6398c8fff837153b6a3e256fd6f92c9b
Natali <n_sunberry@inbox.ru>
2022-08-16 17:17:51 +0300

    8 Задача

--- Practice8/Program.cs
+++ Practice8/Program.cs
@@ -1,1 +1,10 @@
-﻿// 
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int s = 0;
+while (n > 0)
+    {
+        s = s + (n % 10);
+        n = n / 10;
+    }
+Console.WriteLine(s);
